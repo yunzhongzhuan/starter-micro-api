82c0cf7553f3adc68ab7f8daa199a4d816fc8c4f
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2024-02-02 15:30:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
-     console.log(`Just got a request at ${req.url}!`);
-    res.write(await fetch("https://example.com/movies.json"));
-    res.end();
+     //console.log(`Just got a request at ${req.url}!`);
+     const response = await fetch("http://example.com/movies.json");
+     console.log(response);
+     res.write("123");
+     res.end();
 }).listen(process.env.PORT || 3000);
